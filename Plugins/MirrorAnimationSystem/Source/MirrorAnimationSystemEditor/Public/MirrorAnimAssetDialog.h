dd5171c5d3fb3888dfff7965eda7588c4a69476a
Giri <pbgiridhara@gmail.com>
2020-04-18 15:34:37 -0700

    Added Plugins and ALS_Base_Character_BP

---
+++ Plugins/MirrorAnimationSystem/Source/MirrorAnimationSystemEditor/Public/MirrorAnimAssetDialog.h
@@ -0,0 +1,44 @@
+// Copyright 2017-2018 Theodor Luis Martin Bruna (Hethger). All Rights Reserved.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "Input/Reply.h"
+#include "Widgets/DeclarativeSyntaxSupport.h"
+#include "Widgets/SCompoundWidget.h"
+#include "Engine/Texture2D.h"
+#include "IDetailsView.h"
+#include "Animation/AnimSequence.h"
+#include "MirrorAnimAssetSettings.h"
+
+/*Dialog used to choose the Mirror Table,
+it then Mirrors the animation according to the data contained in the Mirror Table*/
+class MIRRORANIMATIONSYSTEMEDITOR_API SMirrorAnimAssetDialog : public SCompoundWidget
+{
+public:
+
+	SLATE_BEGIN_ARGS(SMirrorAnimAssetDialog) {}
+	SLATE_END_ARGS()
+
+		void Construct(const FArguments& InArgs, UAnimSequence* AnimSequence);
+
+	~SMirrorAnimAssetDialog();
+
+	static bool ShowWindow(UAnimSequence* SourceAnimSequence);
+
+	static void CreateMirrorSequenceFromAnimSequence(UAnimSequence* MirrorSequence, UMirrorTable* MirrorTable);
+private:
+
+	UAnimSequence* SourceAnimSequence;
+
+	class UMirrorAnimAssetSettings* MirrorAnimAssetSettings;
+
+	FReply MirrorClicked();
+
+	FReply CancelClicked();
+
+	void CloseContainingWindow();
+
+	void CreateMirroredAnimSequences();
+
+	TSharedPtr<class IDetailsView> MainPropertyView;
+};
