c60b36d30dfcf5f2ec97fd2849a1aaadc56ffd47
Giri <pbgiridhara@gmail.com>
2020-04-19 03:40:13 -0700

    Fixed Wwise

--- Plugins/MirrorAnimationSystem/Source/MirrorAnimationSystem/Private/MirrorTable.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2018 Theodor Luis Martin Bruna (Hethger). All Rights Reserved.
-#include "MirrorTable.h"
-#include "MirrorAnimationSystem.h"
-
-
-
-
-
--- Plugins/MirrorAnimationSystem/Source/MirrorAnimationSystemEditor/Private/MirrorTableFromSkeletonSettings.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2018 Theodor Luis Martin Bruna (Hethger). All Rights Reserved.
-#include "MirrorTableFromSkeletonSettings.h"
-#include "MirrorAnimationSystemEditor.h"
-
-
-UMirrorTableFromSkeletonSettings::UMirrorTableFromSkeletonSettings(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-}
